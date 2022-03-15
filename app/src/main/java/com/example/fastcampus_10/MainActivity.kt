f4a9dc03e40475ca8175aa95bbad4671d2be5f33
LimJaeHyung96 <jhlim96@naver.com>
2022-03-15 15:42:36 +0900

    220315 commit

--- app/src/main/java/com/example/fastcampus_10/MainActivity.kt
+++ app/src/main/java/com/example/fastcampus_10/MainActivity.kt
@@ -2,12 +2,24 @@ package com.example.fastcampus_10
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
 import androidx.viewpager2.widget.ViewPager2
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
+import org.json.JSONArray
+import org.json.JSONObject
+import kotlin.math.absoluteValue
 
 class MainActivity : AppCompatActivity() {
 
-    private val viewPager : ViewPager2 by lazy {
+    private val viewPager: ViewPager2 by lazy {
         findViewById(R.id.viewPager)
+    }
+
+    private val progressBar : ProgressBar by lazy {
+        findViewById(R.id.progressBar)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,10 +27,72 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         initViews()
+        initData()
     }
 
     private fun initViews() {
-        viewPager.adapter = QuotePagerAdapter(emptyList())
+        viewPager.setPageTransformer { page, position ->
+            when {
+                position.absoluteValue>= 1.0F -> {
+                    page.alpha= 0F
+                }
+                position == 0F -> {
+                    page.alpha = 1F
+                }
+                else -> {
+                    page.alpha = 1F - (position.absoluteValue * 2)
+                }
+            }
+        }
+    }
+
+    private fun initData() {
+        val remoteConfig = Firebase.remoteConfig
+
+        //원래는 한 번하고 12시간 뒤에 수정이 이루어지는데 개발하면서 확인하기 위해서 그 텀을 0으로 줄임
+        //서버에서 블락하지 않으면 바꾸는 내용이 바로 적용됨
+        remoteConfig.setConfigSettingsAsync(
+            remoteConfigSettings {
+                minimumFetchIntervalInSeconds = 0
+            }
+        )
+
+        remoteConfig.fetchAndActivate().addOnCompleteListener {
+            progressBar.visibility = View.GONE
+            if (it.isSuccessful) {
+                val quote = parseQuotesJson(remoteConfig.getString("quotes"))
+                val isNameReveal = remoteConfig.getBoolean("is_name_reveal")
+                displayQuotesPager(quote, isNameReveal)
+            }
+        }
+    }
+
+    private fun parseQuotesJson(remoteConfigJson: String): List<Quote> {
+        val jsonArray = JSONArray(remoteConfigJson)
+        var jsonList = emptyList<JSONObject>()
+        for (index in 0 until jsonArray.length()) {
+            val jsonObject = jsonArray.getJSONObject(index)
+            jsonObject?.let {
+                jsonList = jsonList + it
+            }
+        }
+
+        return jsonList.map {
+            Quote(
+                quote = it.getString("quote"),
+                name = it.getString("name")
+            )
+        }
+    }
+
+    private fun displayQuotesPager(quote: List<Quote>, isNameReveal : Boolean) {
+        val adapter = QuotePagerAdapter(
+            quotes = quote,
+            isNameRevealed = isNameReveal
+        )
+
+        viewPager.adapter= adapter
+        viewPager.setCurrentItem(adapter.itemCount / 2 + 2 , false)
     }
 
 }
--- app/src/main/java/com/example/fastcampus_10/QuotePagerAdapter.kt
+++ app/src/main/java/com/example/fastcampus_10/QuotePagerAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.fastcampus_10
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,7 +8,8 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class QuotePagerAdapter(
-    private val quotes: List<Quote>
+    private val quotes: List<Quote>,
+    private val isNameRevealed : Boolean
 ) : RecyclerView.Adapter<QuotePagerAdapter.QuoteViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
@@ -17,19 +19,26 @@ class QuotePagerAdapter(
         )
 
     override fun onBindViewHolder(holder: QuoteViewHolder, position: Int) {
-        holder.bind(quotes[position])
+        val actualPosition = position % quotes.size
+        holder.bind(quotes[actualPosition], isNameRevealed)
     }
 
-    override fun getItemCount() = quotes.size
+    override fun getItemCount() = Int.MAX_VALUE //계속 넘기다보면 결국에는 끝은 있음
 
     class QuoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         private val quotoTextView: TextView = itemView.findViewById(R.id.quoteTextView)
         private val nameTextView: TextView = itemView.findViewById(R.id.nameTextView)
 
-        fun bind(quote: Quote) {
-            quotoTextView.text = quote.quote
-            nameTextView.text = quote.name
+        @SuppressLint("SetTextI18n")
+        fun bind(quote: Quote, isNameRevealed: Boolean) {
+            quotoTextView.text = "\"${quote.quote}\""
+            if(isNameRevealed) {
+                nameTextView.text = "-${quote.name}"
+                nameTextView.visibility = View.VISIBLE
+            } else {
+                nameTextView.visibility = View.INVISIBLE
+            }
         }
     }
 
